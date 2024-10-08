36736fe904378a976458e18d8b0e35840864d3b4
FranSC08 <69852225+FranSC08@users.noreply.github.com>
2024-10-07 22:18:18 -0300

    Add files via upload

---
+++ exer1.java
@@ -0,0 +1,15 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+// The main method must be in a class named "Main".
+class Main {
+    public static void main(String[] args) {
+    Scanner leia = new Scanner(System.in);
+        String nome;
+
+        nome = leia.nextLine();
+
+        System.out.printf("Seu nome e" + nome);
+    }
+}
